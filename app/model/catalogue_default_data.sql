d2b9604522475794616a2c779f1778b73bd50c88
Tim <tim.bathgate@gmail.com>
2016-12-09 13:30:29 +1300

    Refactor to use computed URLs (i.e url_for)

--- app/model/catalogue_default_data.sql
+++ app/model/catalogue_default_data.sql
@@ -1,10 +1,10 @@
 INSERT INTO categories (name, image_path) VALUES
-  ('Art', 'static/img/art.png'),
-  ('Architecture', 'static/img/architecture.png'),
-  ('Furniture', 'static/img/furniture.png'),
-  ('Design', 'static/img/design.png'),
-  ('Typography', 'static/img/typography.png'),
-  ('People', 'static/img/people.png');
+  ('Art', 'img/art.png'),
+  ('Architecture', 'img/architecture.png'),
+  ('Furniture', 'img/furniture.png'),
+  ('Design', 'img/design.png'),
+  ('Typography', 'img/typography.png'),
+  ('People', 'img/people.png');
 
 INSERT INTO examples (category_id,
                       name,
@@ -14,12 +14,12 @@ INSERT INTO examples (category_id,
                       creator_email) VALUES
 
   (1, 'On White II', 'Some detail', 1923,
-   'uploads/on-white-ii-1923.jpeg', 'emma.kovacs@gmail.com'),
+   'on-white-ii-1923.jpeg', 'emma.kovacs@gmail.com'),
     (1, 'The Dance I', 'Some detail', 1909,
-   'uploads/on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com'),
+   'on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com'),
     (1, 'Composition II in Red, Blue, and Yellow', 'Some detail', NULL,
-   'uploads/Newman-Whos_Afraid_of_Red,_Yellow_and_Blue.jpg', 'tim.bathgate@gmail.com'),
+   'Newman-Whos_Afraid_of_Red,_Yellow_and_Blue.jpg', 'tim.bathgate@gmail.com'),
     (1, 'Number 3', 'Some detail', NULL,
-   'uploads/on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com'),
+   'on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com'),
     (1, 'No. 61 (Rust and Blue)', 'Some detail', 1953,
-   'uploads/on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com');
+   'on-white-ii-1923.jpeg', 'tim.bathgate@gmail.com');
